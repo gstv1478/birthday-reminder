e60c1d163390ff4682d982511c6ff8c36d2a0c81
gstv1478 <gstv1478@gmail.com>
2022-12-31 02:59:19 -0300

    primeiro commit

---
+++ src/List.js
@@ -0,0 +1,11 @@
+
+
+const List = () => {
+    return (
+        <>
+            <h2>list component</h2>      
+        </>
+    );
+};
+
+export default List
---
+++ src/data.js
@@ -0,0 +1,39 @@
+ var data = [
+    {
+      id: 1,
+      name: 'Lavignia Lima',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    },
+    {
+      id: 2,
+      name: 'Larissa Suellen',
+      age: 32,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+      id: 3,
+      name: 'Daniel Freitas',
+      age: 36,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+      id: 4,
+      name: 'Arthur Alvim',
+      age: 34,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+      id: 5,
+      name: 'Alessandra Santos',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+  ];
+
+  export default data
